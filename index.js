0d5750da584cff4b233c371e325f3150f5670400
Muhammad Usaid <osaid4624@users.noreply.github.com>
2024-06-17 04:30:58 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,147 @@
+"use strict";
+//Assiment No1 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// const name = "Hello Eric";
+// console.log(`${name}  "would you like to learn some Python today?"`);
+//Assiment complete
+Object.defineProperty(exports, "__esModule", { value: true });
+//Assiment No2 Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// const name = "Eric"
+// console.log(name.toLocaleUpperCase());
+// console.log(name.toLocaleLowerCase());
+// console.log(name.charAt(0).toLocaleUpperCase()+name.slice(1).toLowerCase());
+//Complete
+//Assiment No3 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// let name = "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
+// console.log(name);
+// //Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// let famous_person = "Albert Einstein";
+// console.log(`${famous_person}  once said, “A person who never made a mistake never tried anything new.”`);
+//Assment No 4 
+// const name: string = "\t\n Asharib \t\n"; // This saves the name with whitespace
+// console.log( name);
+// console.log(name.trim());
+//complete 
+//Assiment No 5
+//  console.log(5+3);
+//  console.log(2*4);
+//  console.log(10-2);
+//  console.log(16/2);
+// complete 
+//Assiment No6 
+//  let num:number = 5;
+//  let num1:number = 3;
+//  console.log(`my favroit number is ${num}`);
+//Assoment compelte 
+//Assiment No 7 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Name Usaid // 23/5/24
+// let name = "usaid " 
+// console.log(`Hello ${name}I'm very enjoy to learn typescipt`);
+// Complete 
+// Assiment No 8 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//  let name:string[]=['usaid','ali','hamza','hussain']
+//     for(let i=0; i<name.length; i++)
+//     console.log(name[i]); // Jo specific name show karwna ha uska index number likho
+// Complete 
+//Assimemt No 9 
+// let names :string[]=['usaid','ali','hamza','hussain']
+// for (let name of names){
+//     console.log(`${name} "would you like to learn typescript`);
+// }
+//Assiment complete
+//Assiment No 10 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+// Use a for each loop
+// let car :string[]=['audi','honda','toyota','civic']
+// car.forEach(car=>{
+//     console.log(`I would like to own a ${car}`);
+// })
+//Complete
+//Assiment No11 
+//USe a for each loop
+//  let gussetName :string[]=['Usaid','Ali','Hamza','Huzaifia']
+//  gussetName.unshift("Nikola Tesla")
+//  gussetName.splice(2,0,"John")
+//  gussetName.push("david")
+//  gussetName.forEach(gussetName=>{
+//      console.log(`Dear ${gussetName}, would you like to join me for dinner?`);
+//  })
+//Complete
+// Assiment 11    //Discord assiment 15
+// let unabletoAttande="Usaid"
+// console.log(`${unabletoAttande} can ,make it a dinner`);
+// // Replace the guesst 
+// let newGuesst="hamza";
+// gussetName[gussetName.indexOf(unabletoAttande)]= newGuesst 
+// // New Invatition
+// gussetName.forEach(gussetName=>{
+//     console.log(`Dear ${gussetName} would you like to join my partry`);
+// })
+// //complete
+//Assiment 12
+//  let name:string []=['usaid','asad','hamza','John']
+//  console.log("Great News For add New Guesst !");
+//  // Adding more gusset  name.unshift("Issac Newton")
+//  name.splice(2,0, "Hussain")
+//  name.push("Baber")
+//  name.forEach(name=>{
+//      console.log(`Dear ${name} Would You like to Join Dinner with me ?`);
+//  })
+//  //complete 
+// // Assiment 13
+// console.log("Ohh sorry unfortunately I'm invite only two friends Sorry");
+// // While loop
+// while(name.length<2){
+//     let removeGuests = name.pop()
+//     console.log(`Sorry ${removeGuests} I can't invite you `);
+// // for each loop 
+// name.forEach(name=>{
+//     console.log(`Dear ${name} Invite only you So I'm waiting for Today Dinner !`)
+// })}
+// name.splice(2,name.length)
+// console.log(name);
+// Assiment 14 
+// let listName:string[]=['hello world','Today is weather is very beatuiful','I going to cenima ','I start a learn typscript',]
+// console.log("Orignal Order", listName);
+//  console.log("Alpebatic Order", [...listName].sort());
+//  console.log("Orginal Order", listName);
+//  console.log("Revers Alphabet",[...listName].sort().reverse());
+//  console.log("Orignal order ", listName);
+// listName.reverse();
+// console.log("Reverse order",listName);
+// listName.reverse()
+// console.log("orginal order", listName);
+// listName.sort()
+// console.log("Sort order Name ", listName);
+// listName.reverse()
+// console.log("revers alphabatic ", listName);
+// complete
+//Assiment 15 
+// let gueestName:string[]=['Usaid',"hamza","Obaid","Ali"]
+// console.log(` I'm inviting  ${gueestName.length} people to fiends `);
+// complete 
+// Asssiment 16
+// let listName:string[]=['United Kindom','canada','USA']
+// console.log("My favriot Country ", listName);
+//complete 
+//Assimeent 17 Disscord 21 
+// let newCar :{model:string, color:string, year:number,owner:string,}={
+//     model:"Civic",
+//     color:"Black",
+//     year:2023,
+//     owner:"Usaid",
+// }
+// console.log(`my car model ${newCar.model} I'm custumize my favroit colore ${newCar.color} My new car model year ${newCar.year} Car owner name is ${newCar.owner}`);
+//complete
+//Assiment 18
+// Assimnet 19 Disscord 22 
+//Array index errors happen when you try to access a position in the array that doesn’t exist. Experimenting with this helps you understand array boundaries.
+// let friends:string[]=['usaid','hamza','ali']
+// //rong besuse not intalize a 3 index numbner not given an array 3 becuse not provide a value of 3rd index number  alwas start a number 0 index number 
+// // console.log(friends[3]); //// Intentional error: Arrays are zero-indexed, so index 3 is out of bounds.
+// console.log(friends[2]); //solve error
+// friends[2] //// Correcting the error by accessing a valid index.
+//Assiment 20 Disscord 23
+let carName = 'Audi';
+console.log("My car === Audi I preduct true ?");
+console.log(carName == 'Audi');
+console.log('Not my car === Mehran I predict false');
+console.log(carName == "Mehran");
